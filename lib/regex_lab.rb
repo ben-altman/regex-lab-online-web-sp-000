6dafd01b0cf18d37a9437dea53860f74d721c1fe
Ben Altman <altmanguitar@gmail.com>
2020-09-01 14:04:20 +0000

    Automatically backed up by Learn

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -15,7 +15,6 @@ def first_word_capitalized_and_ends_with_punctuation?(text)
 end
 
 def valid_phone_number?(phone)
-  if phone.scan(/\d{10}/x) true
-  else false
-  end
+  phone.scan(/\d/x).count
+  
 end
